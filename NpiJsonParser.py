f51d419a8d6517d8daa28ed3e3f68405efe073f4
Aditya Inapurapu <aialerts@yahoo.com>
2018-07-26 15:52:05 -0700

    Formatting

--- NpiJsonParser.py
+++ NpiJsonParser.py
@@ -5,10 +5,11 @@ def main():
 	url = "https://npiregistry.cms.hhs.gov/api?number=1285005603"
 	r = requests.get(url)
 	print (r.status_code)
+
 	jsonString = json.loads(r.text)
 	first_name = jsonString['results'][0]['basic']['first_name']
 	last_name = jsonString['results'][0]['basic']['last_name']
 	print (first_name)
-	print (last_name)
+	print last_name
 
 main()
